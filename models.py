fbdf65497aaf561dc702105b060641bbae9cb979
HasnainRaz <hasnain3257@gmail.com>
2020-02-19 22:29:29 +0100

    change upsampling

--- models.py
+++ models.py
@@ -104,11 +104,11 @@ class ResnetGenerator(nn.Module):
 
         for i in range(n_downsampling):  # add upsampling layers
             mult = 2 ** (n_downsampling - i)
-            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
-                                         kernel_size=3, stride=2,
-                                         padding=1, output_padding=1,
-                                         bias=use_bias),
-                      norm_layer(int(ngf * mult / 2)),
+            model += [nn.UpsamplingBilinear2d(scale_factor=2),
+                      norm_layer(ngf * mult),
+                      nn.ReLU(True)]
+            model += [nn.Conv2d(ngf * mult, ngf * mult, kernel_size=3, stride=1, padding=1),
+                      norm_layer(ngf * mult),
                       nn.ReLU(True)]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
